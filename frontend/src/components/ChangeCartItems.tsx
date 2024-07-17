c69dfd440c0a76e1be505669ee404f0c57e4acd5
Mia <mia.mecklenburg@gmx.net>
2024-07-17 10:20:56 +0200

    add item and remove item from cart

--- frontend/src/components/ChangeCartItems.tsx
+++ frontend/src/components/ChangeCartItems.tsx
@@ -1,7 +1,10 @@
 import { CiSquareMinus } from "react-icons/ci";
 import { CiSquarePlus } from "react-icons/ci";
-import { useQuery, gql } from '@apollo/client';
+import { IconContext } from "react-icons";
+import { useQuery, gql, useMutation } from '@apollo/client';
+import LoadingIcon from "./LoadingIcon";
 
+// get all cart-items to check if this product is already in the cart
 const GET_CART = gql`
     query {
         cart {
@@ -14,28 +17,80 @@ const GET_CART = gql`
     }
     `
 
+// add new item to cart
+const ADD_TO_CART = gql`
+  mutation AddToCart($productId: ID!) {
+    addToCart(productId: $productId) {
+        items {
+            product {
+                id
+            }
+            quantity
+            }
+        }
+    }
+`;
+
+// remove item from cart
+const REMOVE_FROM_CART = gql`
+  mutation RemoveFromCard($productId: ID!) {
+    removeFromCart(productId: $productId) {
+        items {
+            product {
+                id
+                }
+            quantity
+            }
+        total
+        }
+    }
+`;
+
 const ChangeCartItems = ({productId}:{productId: string}) => {
-    const {loading, error, data} = useQuery(GET_CART)
+    // check cart
+    const {loading: queryLoading, error: queryError, data} = useQuery(GET_CART)
+    const isItemInCart = data?.cart?.items.some((singleItem: any) => singleItem.product.id === productId)    
 
-    if(loading) return <p>Loading</p>
-    if(error) return <p>Something went wrong</p>
+    // add to cart
+    const [addToCart, { loading: addLoading, error: addError }] = useMutation(ADD_TO_CART);
+    const handleAddItem = async () => {
+        try {
+          await addToCart({ variables: {productId} });
+        } catch (e) {
+          console.error(e);
+        }
+      };
+    
+    // remove from cart
+    const [removeFromCart, { loading: removeLoading, error: removeError }] = useMutation(REMOVE_FROM_CART);
+    const handleRemoveItem = async () => {
+        try {
+          await removeFromCart({ variables: {productId} });
+        } catch (e) {
+          console.error(e);
+        }
+      };
 
-    const isItemInCart = data.cart.items.some((singleItem: any) => singleItem.product.id === productId)    
+
+    if( queryLoading || addLoading || removeLoading ) return <LoadingIcon/>
+    if( queryError || addError || removeError ) return <p>Something went wrong</p>
 
     return ( 
-    <div className="flex gap-3">
-        {isItemInCart ? 
-            <button className="cursor-pointer">
-                <CiSquareMinus />
-                <p>Remove from cart</p>
-            </button>
-            :
-            <button className="cursor-pointer">
-                <CiSquarePlus />
-                <p>Add to cart</p>
-            </button>
-        }
-    </div> );
+    <IconContext.Provider value={{ size: "1.2rem" }}>
+        <div className="flex gap-3 relative">
+            {isItemInCart ? 
+                <button className="cursor-pointer absolute right-0" onClick={handleRemoveItem}>
+                    <CiSquareMinus />
+                </button>
+                :
+                <button className="cursor-pointer absolute right-0" onClick={handleAddItem}>
+                    <CiSquarePlus />
+                    <p>Add to cart</p>
+                </button>
+            }
+        </div>
+    </IconContext.Provider>
+    );
 }
  
 export default ChangeCartItems;
